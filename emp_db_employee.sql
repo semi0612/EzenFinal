5279b7e79a9d1b971baad70bb83264c81b94b71f
Jeeyool <80949244+Jeeyool@users.noreply.github.com>
2021-07-02 15:32:55 +0900

    백업

--- emp_db_employee.sql
+++ emp_db_employee.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `employee`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `employee` (
   `EMP_ID` varchar(45) NOT NULL,
-  `EMP_PW` varchar(45) DEFAULT NULL,
+  `EMP_PW` varchar(100) DEFAULT NULL,
   `EMP_NAME` varchar(45) NOT NULL,
   `EMP_EMAIL` varchar(45) NOT NULL,
   `EMP_TEL` varchar(45) DEFAULT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES ('2013270073','ntkc4xtw','김쿠키','cookie@naver.com','02-2242-2242','IT기획팀','과장',0,'2021-06-28 01:21:25',NULL,'4200','cookie.jpg',19,0),('20201003','wldbf','민지율','jeey0506@naver.com','070-4654-5575','개발1팀','사원',0,'2021-02-25 00:00:00',NULL,'2800',NULL,15,0),('20245642','tgdwdg1u','이제훈','jeey0506@naver.com','070-2827-1123','인사총무팀','대리',1,'2021-06-28 02:14:01',NULL,'3800','149145568045455.jpg',19,0),('2045878','yoydtjux','김연아','jeey0506@naver.com','6472126024','개발2팀','주임',0,'2021-06-27 22:43:49',NULL,'4300','DDP.jpg',15,0),('424222225','tqap2g7i','김군찬','jeey0506@naver.com','070-3334-2223','공정안전팀','대리',0,'2021-06-28 02:47:19',NULL,'3800','v3_0916311.png',18,0),('42454542','yoydtjux','김연아','jeey0506@naver.com','070-3838-2827','구매무역팀','주임',1,'2021-06-28 02:06:02',NULL,'3800','08pt9k5qa857u85k8387.jpg',17,0),('46556646','6sbkrfig','김지윤','jeey0506@naver.com','070-2822-1111','공정안전팀','팀장',0,'2021-06-28 03:16:56',NULL,'4500','v3_0910735.jpg',20,0);
+INSERT INTO `employee` VALUES ('022210','wldbf','민지율','jeey0506@naver.com','070-4654-5575','개발1팀','사원',0,'2021-02-25 00:00:00',NULL,'2800',NULL,15,0),('072210','$2a$10$y87LBsLWb.U0etGZlalGLu.mR/HQN7Yd1Cc.sirkDtZFoeA4GBFc.','정재영','jeey0506@naver.com','070-3837-2812','재경팀','사원',0,'2021-07-02 14:36:43',NULL,'4300','정재영.png',19,0),('113210','$2a$10$MApLjnYQ/Nn1rcnTwFvLQ.ran0LxiQWS2nMq15wi/68CAhTT5EMtO','이현수','jeey0506@naver.com','070-2821-2221','영업1팀','주임',0,'2021-07-01 19:16:24',NULL,'4300','generated_photos_5f1807ec1f67b0000679fe00.jpg',19,0),('2013270073','ntkc4xtw','김쿠키','cookie@naver.com','02-2242-2242','IT기획팀','과장',0,'2021-06-28 01:21:25',NULL,'4200','cookie.jpg',19,2),('20245642','tgdwdg1u','이제훈','jehooon@naver.com','070-2827-1123','인사총무팀','대리',1,'2021-06-28 02:14:01',NULL,'3800','149145568045455.jpg',19,0),('2045878','yoydtjux','김연아','jeey0506@naver.com','070-1234-3333','개발2팀','주임',0,'2021-06-27 22:43:49',NULL,'4300','DDP.jpg',15,0),('424222225','tqap2g7i','김군찬','goonchan24@naver.com','070-3334-2223','공정안전팀','대리',0,'2021-06-28 02:47:19',NULL,'3800','v3_0916311.png',18,1),('42454542','yoydtjux','임윤아','jeey0506@naver.com','070-3838-2827','인사총무팀','주임',1,'2021-06-28 02:06:02',NULL,'3800','08pt9k5qa857u85k8387.jpg',17,0),('46556646','6sbkrfig','김지윤','wldbs012@naver.com','070-2822-1111','구매무역팀','팀장',0,'2021-06-28 03:16:56',NULL,'4500','v3_0910735.jpg',20,0);
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -60,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-28 12:25:55
+-- Dump completed on 2021-07-02 15:31:27
